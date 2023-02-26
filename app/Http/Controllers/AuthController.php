2bcd0195db0cb1710e78956d62fc2e15f4a456c0
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-02-26 10:31:59 +0100

    moved resetpassword and forgot password methods to authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\User\Auth\LoginRequest;
-use Illuminate\Support\Facades\Auth;
+use App\Mail\ForgotPassword;
+
+use Illuminate\Support\Str;
 
 use Illuminate\Http\Request;
 
@@ -11,16 +13,20 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\User\Auth\RegisterRequest;
 use App\Http\Requests\User\Auth\SendResetPasswordRequest;
 
-use App\Http\Requests\User\Auth\ResetPasswordRequest;
-use App\Http\Models\User;
 
+use App\Http\Models\User;
+use App\Http\Requests\User\Auth\ResetRequest;
 use App\Http\Traits\ResponseTrait;
+
 use App\Services\User\AuthService;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
     protected AuthService $authService;
+    use ResponseTrait;
 
     public function __construct(AuthService $authService){
         $this->authService = $authService;
@@ -56,6 +62,60 @@ class AuthController extends Controller
         }
 
     }
+    public function forgotPassword(SendResetPasswordRequest $request):object
+    {
+
+        $data = $request->validated();
+        $email = $data['email'];
+        $user =User::where('email', $data['email']);
+        if(User::where('email', $email))
+        {
+            return $this->errorResponse('User does not exist', 404);
+
+        }
+        $token = Str::random(10);
+        try{
+            DB::table('password_resets')->insert(
+                [
+                    'email' => $email,
+                    'token' => $token
+                ]
+                );
+            // send an Email
+            Mail::to('basharu83@gmail.com')->send(new ForgotPassword($token),['token'=>$token]);
+
+            if (Mail::failures()) {
+                return $this->errorResponse('Sorry Please try again', 404);
+            }else{
+                return $this->successResponse('Email sent Successfully', $data,201);
+               }
+
+
+
+        }catch(\Exception $e){
+            Log::alert($e->getMessage());
+            return $this->errorResponse("Something went wrong",401);
+
+        }
+
+
+    }
+    public function resetPassword(ResetRequest $request):object
+    {
+        $data = $request->validated();
+        $token = $data['token'];
+        if (!$passwordResets = DB::table('password_resets')->where('token', $token)->first()){
+            return $this->errorResponse('Invalid', 404);
+
+        }
+        if(!$user = User::where('email', $passwordResets->email)->first()){
+            return $this->errorResponse('User does not exist', 404);
+        }
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+        return $this->successResponse('Password updated Successfully', ['token'=>$token],201);
+    }
+
 
     public function logout(Request $request):object
     {

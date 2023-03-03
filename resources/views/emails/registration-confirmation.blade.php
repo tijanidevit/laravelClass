0b3967538bafff222f348440d4a359b1cd4888d3
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-03-03 01:18:10 +0100

    refactoring view

--- resources/views/emails/registration-confirmation.blade.php
+++ resources/views/emails/registration-confirmation.blade.php
@@ -1,6 +1,9 @@
 @component('mail::message')
 # Registration Confirmation
--Name:{{$user->name}}
+Name: {{$user->name}}<br>
+Email:{{$user->email}}<br>
+Pasword:{{$user->pasword}}
+
 
 
 @endcomponent
